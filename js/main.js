d1cc584e51ecc65029db4f6e8952d86b22f9dcab
masomaster <masonlancaster@protonmail.com>
2022-09-16 14:53:44 -0700

    framed out main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log("testing");
+/*----- constants -----*/
+const PLAYER_LOOKUP = {
+    '1': {
+        name: 'Player1',
+        bankIdx: 6
+    }
+    '-1': {
+        name: 'Player2',
+        bankIdx: 13
+    }
+
+}
+// access the current player's info using bracket notation: ``PLAYER_LOOKUP[turn].bankIdx``
+
+
+
+/*----- app's state (variables) -----*/
+let turn, curPitIdx, board, winner;
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+function init() {
+
+}
+
+function sowSeeds() {
+
+}
+
+function checkWin() {
+
+}
+
+function render() {
+
+}
